303bbe5b519cdce7039f8f9782e0b7da293a8444
Afaq <afaqpubg123@gmail.com>
2021-06-21 17:16:37 +0500

    updated

--- app/src/main/java/com/example/dotfitness/MainActivity.java
+++ app/src/main/java/com/example/dotfitness/MainActivity.java
@@ -1,24 +1,35 @@
 package com.example.dotfitness;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    ConstraintLayout lay_workout,lay_bmi;
+    LinearLayout act_bmi, act_exercises;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lay_workout = findViewById(R.id.lay_workout);
-        lay_bmi = findViewById(R.id.lay_bmi);
+        act_bmi = findViewById(R.id.lay_bmi);
+        act_exercises = findViewById(R.id.lay_exercises);
+
+        act_exercises.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ExercisesActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
-        lay_bmi.setOnClickListener(new View.OnClickListener() {
+        act_bmi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, BmiActivity.class);
@@ -26,13 +37,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        lay_workout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ExercisesActivity.class);
-                startActivity(intent);
-            }
-        });
 
     }
 }
